95dc4aeabf5f045b369165f11000a1b2153155bb
apduartte <apduartte@gmail.com>
2021-09-26 23:21:02 -0300

    Formulario
    Criação de formulário

---
+++ cadastro simples/script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+      <head>
+         <title> Alert JavaScripr </title>
+         <script>
+          function funcao1()
+          {
+         alert ("Mensagem Recebida!");
+         }
+         </script>
+      </head>
+   <body>
+      <input type ="button" onclick="funcao1()" value ="Exibir Alert"/>
+   </body>
+</html>
